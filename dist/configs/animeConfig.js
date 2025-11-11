a23540ddd1798f73427d77a52883bdfa1c6dcb3f
ridantegar <tegarridwan82@gmail.com>
2025-11-12 00:38:51 +0700

    first commit

---
+++ dist/configs/animeConfig.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const animeConfig = {
+    PORT: 3001,
+    baseUrl: {
+        otakudesu: "https://otakudesu.best",
+        samehadaku: "https://samehadaku.mba",
+    },
+    response: {
+        href: true,
+        sourceUrl: true,
+    },
+};
+exports.default = animeConfig;
